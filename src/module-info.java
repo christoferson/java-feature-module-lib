0812745d63d50072ab7746b331d10e306fecf132
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-07-09 12:01:30 +0900

    Add blank account service implementations

--- src/module-info.java
+++ src/module-info.java
@@ -1,4 +1,9 @@
+import demo.service.IAccountService;
+import demo.service.impl.AccountServiceJpa;
+import demo.service.impl.AccountServiceMemory;
+
 module demo.module.lib {
 	exports demo.model;
 	exports demo.service;
+	provides IAccountService with AccountServiceMemory, AccountServiceJpa;
 }
