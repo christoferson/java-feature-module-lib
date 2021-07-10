9158e8df32799a96007a97770ab90309dc8f376f
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-07-10 23:42:50 +0900

    Add Service Implementation export for Sql

--- src/module-info.java
+++ src/module-info.java
@@ -1,9 +1,10 @@
 import demo.service.IAccountService;
 import demo.service.impl.AccountServiceJpa;
 import demo.service.impl.AccountServiceMemory;
+import demo.service.sql.AccountServiceSql;
 
 module demo.module.lib {
 	exports demo.model;
 	exports demo.service;
-	provides IAccountService with AccountServiceMemory, AccountServiceJpa;
+	provides IAccountService with AccountServiceMemory, AccountServiceJpa, AccountServiceSql;
 }
